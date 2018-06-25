e842c333f263273db500489ddd070e715d1f4da3
Dorian Helbig <38417651+Lostor44@users.noreply.github.com>
2018-06-25 14:58:51 +0200

    CAn't make it into a package because unity won't start atm

---
+++ Assets/Scripts/PlayerField.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerField{
+
+	public List<Lane> myList = new List<Lane>();
+}
---
+++ Assets/Scripts/Relic Slot.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RelicSlot : ISlot {
+
+	RelicSlot(GameObject x)
+	{
+		gameObject = x;
+	}
+	public GameObject gameObject;
+
+}
