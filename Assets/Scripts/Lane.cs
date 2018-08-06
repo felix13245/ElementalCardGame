693c84a0e72f27e5b18a94a5528f74c7234c5c86
sg <38417796+stywyyy@users.noreply.github.com>
2018-08-06 15:19:33 +0200

    all new

--- Assets/Scripts/Lane.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Lane {
-
-	List<ISlot> myList = new List<ISlot>();
-}
--- Assets/Scripts/PlayerField.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerField{
-
-	public List<Lane> myList = new List<Lane>();
-}
