c590d0354e7a236508843b8a11c0c978b750201c
Yujonokage <38417630+Yujonokage@users.noreply.github.com>
2018-08-08 10:59:23 +0200

    Add files via upload

---
+++ HighlightBorderMouse.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class HighlightBorderMouse : MonoBehaviour
+{
+    public MeshRenderer MouseHighlight;
+
+    void OnMouseEnter()
+    {
+        MouseHighlight.enabled = true;
+    }
+
+    void OnMouseExit()
+    {
+        MouseHighlight.enabled = false;
+    }
+}
